7b0c50fc2e51a5fb08ea765e2c185b0605a759e6
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-29 06:07:23 +0300

    💠 feat: creating users table

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dbConnection from './db/database.js';
+import User from './models/user.model.js';
 
 const app = express();
 
 app.use(express.json());
 
 dbConnection();
+User
 
 const port = 8000;
 app.listen(port, () => {
---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { sequelize } from "../db/database.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User);
+
+
+export default User;
